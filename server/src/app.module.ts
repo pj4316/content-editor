02feeb29ce017bc3a24c79236d45185a39fccd73
maxlee <pj4316@naver.com>
2023-10-09 18:29:00 +0900

    add: export server for docker

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
+      host: process.env.TYPEORM_HOST_URL,
+      port: Number(process.env.TYPEORM_HOST_PORT),
       username: 'root',
       password: 'example',
       database: 'test',
--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,8 @@ import { AllExceptionsFilter } from './filter/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule,  new FastifyAdapter({ logger: true }));
   app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
-  await app.listen(3000);
+  // If you're using Fastify as your server in NestJS instead of the default express server, you'll need to modify the server to listed on 0.0.0.0.
+  // https://www.tomray.dev/nestjs-docker-production
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
